7ed84c3f6fe95da45315ff33242b43462ddaa228
Éderson Lehugeur <eder.lehugeur@gmail.com>
2019-01-29 21:23:52 -0200

    Criação do UserService

---
+++ src/main/java/br/com/project/helpdesk/service/UserService.java
@@ -0,0 +1,40 @@
+package br.com.project.helpdesk.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import br.com.project.helpdesk.entity.User;
+import br.com.project.helpdesk.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public User findByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+
+	public User createOrUpdate(User user) {
+		return userRepository.save(user);
+	}
+
+	public Optional<User> findById(String id) {
+		return userRepository.findById(id);
+	}
+
+	public void delete(String id) {
+		userRepository.deleteById(id);
+	}
+
+	public Page<User> findAll(int page, int count) {
+		PageRequest pageRequest = PageRequest.of(page, count);
+
+		return userRepository.findAll(pageRequest);
+	}
+}
