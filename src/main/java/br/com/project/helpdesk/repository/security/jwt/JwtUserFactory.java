e98e4beb2875125acdfc84156ac107bf32dabd0d
Éderson Lehugeur <eder.lehugeur@gmail.com>
2019-01-30 18:30:35 -0200

    Autenticação e autorização com tokens JWT - Parte 1

---
+++ src/main/java/br/com/project/helpdesk/repository/security/jwt/JwtUserFactory.java
@@ -0,0 +1,28 @@
+package br.com.project.helpdesk.repository.security.jwt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import br.com.project.helpdesk.entity.User;
+import br.com.project.helpdesk.enums.ProfileEnum;
+
+public class JwtUserFactory {
+
+	private JwtUserFactory() {
+	}
+
+	public static JwtUser create(User user) {
+		return new JwtUser(user.getId(), user.getEmail(), user.getPassword(),
+				mapToGrantedAuthorities(user.getProfile()));
+	}
+
+	private static List<GrantedAuthority> mapToGrantedAuthorities(ProfileEnum profileEnum) {
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority(profileEnum.toString()));
+
+		return authorities;
+	}
+}
